0de6515cfd527678183eb8d74f8cf0a5bbb53292
Bill Jaquette <eliminat@HEH.local>
2019-05-01 18:17:15 -0500

    Add project files.

---
+++ TwitchChatBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwitchChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
