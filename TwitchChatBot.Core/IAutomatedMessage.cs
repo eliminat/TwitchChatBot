dada7866a27e3a1bbfff5f88b94916980d26339c
Bill Jaquette <eliminat@gmail.com>
2019-05-01 21:27:32 -0500

    Initial Commit

---
+++ TwitchChatBot.Core/IAutomatedMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchChat.Bot.Core
+{
+    public interface IAutomatedMessage
+    {
+
+        void Initialize(DateTime currentTime);
+
+        bool IsItYourTimeToDisplay(DateTime currentTime);
+
+        string GetMessageInstance(DateTime currentTime);
+    }
+}
--- TwitchChatBot/Program.cs
+++ TwitchChatBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using TwitchChat.Bot.Core;
 
 namespace TwitchChatBot
 {
@@ -6,7 +8,30 @@ namespace TwitchChatBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Initiating ChatBot...");
+            Console.WriteLine("To exit, close window or press Ctrl+C");
+
+            var automatedMessagingSystem = new AutomatedMessagingSystem();
+
+            var intervalTriggeredMessage = new IntervalTriggeredMessage()
+            {
+                DelayInMinutes = 1,
+                Message = "Hello, everyone! I am the bot!"
+            };
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                automatedMessagingSystem.CheckMessages(DateTime.Now);
+
+                while (automatedMessagingSystem.DequeueMessage(out string theMessage))
+                {
+                    Console.WriteLine($"{DateTime.Now.ToShortTimeString()} - {theMessage}");
+                }
+            }
+
         }
     }
 }
